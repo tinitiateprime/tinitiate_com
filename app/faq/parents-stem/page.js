da267df14f7ac26a1ca714c17e014d3fc1b3197f
tinitiateprime <118025881+tinitiateprime@users.noreply.github.com>
2026-01-08 11:59:07 +0530

    Add files via upload

--- app/faq/parents-stem/page.js
+++ app/faq/parents-stem/page.js
@@ -381,8 +381,9 @@
 // File: src/app/parents-faq/page.jsx
 import Link from "next/link";
 import Script from "next/script";
-import faqData from "../../../../public/data/faq/parentsFaqStem.json";
+import faqData from "../../../data/faq/parentsFaqStem.json";
 import FAQList from "../../components/FAQList";
+
 
 export const metadata = {
   title: "Parent FAQ — TINITIATE STEM & IT Programs",
